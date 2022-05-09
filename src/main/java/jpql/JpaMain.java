523f771906e01f3feccf3e8d2a242b725e5e068f
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-05-09 21:50:39 +0900

    jpql

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,51 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션을 받아오지않으면 실제 DB에 저장되지않음
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+
+            /*
+            //Criteria
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            List<Member> resultList = em.createQuery(cq).getResultList();*/
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> q = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> q2 = em.createQuery("select m.username from Member m", String.class);
+            Query q3 = em.createQuery("select m.username, m.age from Member m");
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            //code
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(Member m, Member m1) {
+        System.out.println("m == m2: " + (m1 instanceof Member));
+        System.out.println("m == m2: " + (m instanceof Member));
+    }
+}
